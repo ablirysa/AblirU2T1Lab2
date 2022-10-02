f30bfb1e46ac7232373f4f1f3a22e4e3e7e840c0
ablirysa <ysabellea@nycstudents.net>
2022-10-02 00:23:44 -0400

    updated chatbot and chatbotrunner

---
+++ src/CircleRunner.java
@@ -0,0 +1,12 @@
+public class CircleRunner {
+    public static void main(String[] args) {
+        Circle circ1 = new Circle(5.0);
+        circ1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        // I predict that the code will print out the area of the circle (which should be 78.5).
+
+        // D. run the code and confirm your prediction! Were you correct?
+        // I was correct.
+    }
+}
